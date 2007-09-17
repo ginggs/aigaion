4603333068cf5db6d8a21a6e98ef2e0309b3615e
reidsma <reidsma@74b79808-6e28-0410-ac56-d50eb805f47f>
2007-09-17 22:05:39 +0000

    bit of layout

--- aigaionengine/views/authors/list_items.php
+++ aigaionengine/views/authors/list_items.php
@@ -1,8 +1,13 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <ul class='nosymbol'>
 <?php
+  $initial = '';
   foreach ($authorlist as $author)
   {
+    if ($author->cleanname!='' && strtolower($author->cleanname[0])!=$initial) {
+        $initial = strtolower($author->cleanname[0]);
+        echo '<li><b>'.$author->cleanname[0].'</b></li>';
+    }
     echo "  <li>".anchor('authors/show/'.$author->author_id, $author->cleanname, array('title' => 'All information on '.$author->cleanname))."</li>\n";
   }
 ?>
