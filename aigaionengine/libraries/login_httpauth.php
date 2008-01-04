247e822ee2c3dbbe21efde79fe1df54fba008102
reidsma <reidsma@74b79808-6e28-0410-ac56-d50eb805f47f>
2008-01-04 21:30:10 +0000

    imporved httpauth to work with digest authentication

--- aigaionengine/libraries/login_httpauth.php
+++ aigaionengine/libraries/login_httpauth.php
@@ -1,13 +1,37 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?><?php
 /** Login management from httpauth. All this class needs to do is provide on request a login name or groups
-of somebody currently login according to the external login module (httpauth in this case...). */
+of somebody currently login according to the external login module (httpauth in this case...). 
+Extension for digest auth by Stephan g.A. */
 class Login_httpauth {
     /** Returns an associative array containing the login name of the user and all groups that this user 
     belongs to... (the same names that are stored in aigaion in the abbreviation) */
     function getLoginInfo() {
         //return the proper name
-        return array('login'=>$_SERVER['PHP_AUTH_USER'],'groups'=>array());
+        if(isset($_SERVER['PHP_AUTH_USER'])) {
+            // Basic authentication information can be retrieved from these server variables
+            return array('login'=>$_SERVER['PHP_AUTH_USER'],'groups'=>array());
+        }
+        //requires PHP > 4.1.0
+        if(isset($_SERVER['PHP_AUTH_DIGEST'])) {
+            //Digest authentification
+            $data = $this->http_digest_parse($_SERVER['PHP_AUTH_DIGEST']);
+            return array('login'=>$data['username'], 'groups'=>array());
+        }
+        
+        //fail
+        return array('login'=>'','groups'=>array());
     }
-    
+
+    // function to parse the http auth header into an array of attribute value pairs
+    function http_digest_parse($txt) {
+        $values = array();
+        //this could cause trouble with usernames containing a ','
+        $pairs = explode(", ", $txt);
+        foreach ($pairs as $pair) {
+            $pairValue = explode("=", $pair);
+            $values[$pairValue[0]] = str_replace("\"", "" ,$pairValue[1]);
+        }
+        return $values;
+    }
 }
 ?>
