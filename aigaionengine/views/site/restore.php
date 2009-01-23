37ff61695e8e7852fb44bf75091cf0e041dda91b
reidsma <reidsma@74b79808-6e28-0410-ac56-d50eb805f47f>
2009-01-23 18:10:33 +0000

    added remark on table prefixes on the backup restore page

--- aigaionengine/views/site/restore.php
+++ aigaionengine/views/site/restore.php
@@ -11,6 +11,9 @@ $this->load->helper('form');
 echo "<div class='editform'>";
 echo "<div class='errormessage'>UNSTABLE FUNCTION IN TESTING PHASE. DON'T USE THIS IF YOU DON'T HAVE A GOOD BACKUP OF YOUR LATEST DATA.<br><br>Most importantly, if you don't paste the full contents of an Aigaion 2 backup file here, the database will end up being corrupted.
 </div>";
+echo "<p class='header'>TABLE PREFIX</p>";
+echo "<div class='errormessage'>If the sql code pasted below defines a table prefix different from the prefix defined in this installation of Aigaion, you should change these prefixes in the sql code pasted below, in all; drop, create and insert statements. In the future we will make a more elegant solution for this.
+</div>";
 echo form_open_multipart('site/restorefromsql');
 //formname is used to check whether the POST data is coming from the right form.
 //not as security mechanism, but just to avoid painful bugs where data was submitted 
