fd4e0285e1fc9995565dba993a665509e02337ad
wietseb <wietseb@74b79808-6e28-0410-ac56-d50eb805f47f>
2009-02-26 20:17:23 +0000

    Corrected charset problem: swiched cleanheader to utf8

--- aigaionengine/views/header_clean.php
+++ aigaionengine/views/header_clean.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php $title; ?></title>
     <link href="<?php echo getCssUrl("styling.css"); ?>"     rel="stylesheet" type="text/css" media="screen,projection,tv" />
     <link href="<?php echo getCssUrl("positioning.css"); ?>"     rel="stylesheet" type="text/css" media="screen,projection,tv" />
